657308357881eb6de1f8a6c76161f5f1c7c8fdac
angsongyee <angsongyee@gmail.com>
2023-09-30 22:09:09 +0800

    add login page part 1

--- app.py
+++ app.py
@@ -18,30 +18,35 @@ def chat_with_csv(df,prompt):
     print(result)
     return result
 
-st.set_page_config(layout='wide')
+def login():
+    st.title("Shipping Line Login")
+    st.selectbox("Which shipping line are you from?")
 
-st.title("ChatCSV powered by LLM")
+def chat_page():
+    st.set_page_config(layout='wide')
+    st.title("ChatCSV powered by LLM")
 
-input_csv = st.file_uploader("Upload your CSV file", type=['csv'])
+    input_csv = st.file_uploader("Upload your CSV file", type=['csv'])
 
-if input_csv is not None:
+    if input_csv is not None:
 
-        col1, col2 = st.columns([1,1])
+            col1, col2 = st.columns([1,1])
 
-        with col1:
-            st.info("CSV Uploaded Successfully")
-            data = pd.read_csv(input_csv)
-            st.dataframe(data, use_container_width=True)
+            with col1:
+                st.info("CSV Uploaded Successfully")
+                data = pd.read_csv(input_csv)
+                st.dataframe(data, use_container_width=True)
 
-        with col2:
+            with col2:
 
-            st.info("Chat Below")
-            
-            input_text = st.text_area("Enter your query")
+                st.info("Chat Below")
+                
+                input_text = st.text_area("Enter your query")
 
-            if input_text is not None:
-                if st.button("Chat with CSV"):
-                    st.info("Your Query: "+input_text)
-                    result = chat_with_csv(data, input_text)
-                    st.success(result)
+                if input_text is not None:
+                    if st.button("Chat with CSV"):
+                        st.info("Your Query: "+input_text)
+                        result = chat_with_csv(data, input_text)
+                        st.success(result)
 
+login()
